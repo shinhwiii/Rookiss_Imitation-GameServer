c953c90da75f1bc4340a33bdbdbd178486ff8955
shinhwiii <sh0122451@gmail.com>
2024-04-01 18:50:46 +0900

    feat: Session 구현

--- TestServer/ServerCore/Program.cs
+++ TestServer/ServerCore/Program.cs
@@ -1,29 +1,42 @@
 ﻿using System.Net;
-using System.Net.Sockets;
 using System.Text;
 
 namespace ServerCore
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected: {endPoint}");
+
+            byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server");
+            Send(sendBuff);
+
+            Thread.Sleep(1000);
+
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Client] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+
     class Program
     {
         static Listener _listener = new Listener();
-
-        static void OnAcceptHandler(Socket clientSocket)
-        {
-            // 받는다
-            byte[] recvBuff = new byte[1024];
-            int recvBytes = clientSocket.Receive(recvBuff);
-            string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-            Console.WriteLine($"[From Client] {recvData}");
-
-            // 보낸다
-            byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server");
-            clientSocket.Send(sendBuff);
-
-            // 쫓아낸다
-            clientSocket.Shutdown(SocketShutdown.Both);
-            clientSocket.Close();
-        }
 
         static void Main(string[] args)
         {
@@ -33,7 +46,7 @@ namespace ServerCore
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); // 7777은 포트 번호 (임의로)
 
-            _listener.Init(endPoint, OnAcceptHandler);
+            _listener.Init(endPoint, () => { return new GameSession(); });
             Console.WriteLine("Listening...");
 
             while (true)
