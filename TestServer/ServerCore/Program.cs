b602687aafde9cae8d9930e12c1b50f62f0fff1d
shinhwiii <sh0122451@gmail.com>
2024-04-01 18:44:08 +0900

    feat: 간단한 소켓 프로그래밍 구현

---
+++ TestServer/ServerCore/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DNS (Domain Name System) : www.shinhwi.com => 123.123.123.12
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); // 7777은 포트 번호 (임의로)
+
+            // 문지기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // 문지기 교육
+            listenSocket.Bind(endPoint);
+
+            // 영업 시작
+            listenSocket.Listen(10);    // 10은 최대 대기 수
+
+            while (true)
+            {
+                Console.WriteLine("Listening...");
+
+                // 손님을 입장시킨다
+                Socket clientSocket = listenSocket.Accept();
+
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server");
+                clientSocket.Send(sendBuff);
+
+                // 쫓아낸다
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+    }
+}
